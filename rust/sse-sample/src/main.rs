66024d4bee3a782c57fd36aea994e588547e99ea
will-we <weirson.lee@gmail.com>
2025-07-25 23:24:42 +0800

    sse rust案例

---
+++ rust/sse-sample/src/main.rs
@@ -0,0 +1,61 @@
+use std::convert::Infallible;
+use std::time::Duration;
+use anyhow::Result;
+use axum::response::{Html, IntoResponse, Sse};
+use axum::response::sse::Event;
+use axum::Router;
+use axum::routing::get;
+use axum_extra::{headers, TypedHeader};
+use tokio::net::TcpListener;
+use futures::{stream, Stream};
+use tokio_stream::StreamExt;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
+const INDEX_HTML: &str = include_str!("../static/index.html");
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let addr = "0.0.0.0:6687";
+
+    let app = get_router();
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Listening on: {}", addr);
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+pub fn get_router() -> Router {
+    Router::new()
+        .route("/", get(index_handler))
+        .route("/events", get(sse_handler))
+}
+
+async fn index_handler() -> impl IntoResponse {
+    Html(INDEX_HTML)
+}
+
+pub(crate) async fn sse_handler(
+    TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
+) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    info!("`{}` connected", user_agent.as_str());
+
+    // A `Stream` that repeats an event every second
+    //
+    // You can also create streams from tokio channels using the wrappers in
+    // https://docs.rs/tokio-stream
+    let stream = stream::repeat_with(|| Event::default().data("hi!"))
+        .map(Ok)
+        .throttle(Duration::from_secs(1));
+
+    Sse::new(stream).keep_alive(
+        axum::response::sse::KeepAlive::new()
+            .interval(Duration::from_secs(1))
+            .text("keep-alive-text"),
+    )
+}
