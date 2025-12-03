b6d5b0c5a3030b73fdaf62f104344be8fe5d110d
will-we <weirson.lee@gmail.com>
2025-12-04 01:11:16 +0800

    springboot4 更新JDK

--- java/springboot4-samples/src/main/java/online/willwe/blog/springboot4samples/safenull/SafeNull.java
+++ java/springboot4-samples/src/main/java/online/willwe/blog/springboot4samples/safenull/SafeNull.java
@@ -1,8 +1,13 @@
 package online.willwe.blog.springboot4samples.safenull;
 
+import org.jspecify.annotations.Nullable;
+
 @SuppressWarnings("unused")
 public class SafeNull {
-    public String getName() {
-        return null;
+    public String getUserName(@Nullable String user) {
+        if (user == null) {
+            return "0";
+        }
+        return Integer.toString(user.length());
     }
 }
