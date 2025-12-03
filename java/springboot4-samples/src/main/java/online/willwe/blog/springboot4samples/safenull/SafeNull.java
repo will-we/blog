8b328f15be61f0916197a5e61b038ccacb4ae567
will-we <weirson.lee@gmail.com>
2025-12-03 22:33:55 +0800

    springboot4 null safe 案例

---
+++ java/springboot4-samples/src/main/java/online/willwe/blog/springboot4samples/safenull/SafeNull.java
@@ -0,0 +1,8 @@
+package online.willwe.blog.springboot4samples.safenull;
+
+@SuppressWarnings("unused")
+public class SafeNull {
+    public String getName() {
+        return null;
+    }
+}
---
+++ java/springboot4-samples/src/main/java/online/willwe/blog/springboot4samples/safenull/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package online.willwe.blog.springboot4samples.safenull;
+
+import org.jspecify.annotations.NullMarked;
